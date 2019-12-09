7bf1b94bb6d6eb0c3d9e1cb126522e43cc8a77f3
student 04 <stk_lum_04@outlook.de>
2019-12-09 14:51:40 +0100

    09,12

--- 111/MainWindow.xaml.cs
+++ 111/MainWindow.xaml.cs
@@ -30,15 +30,101 @@ namespace _111
                 for ( int j = 0; j < 3; j++)
                 {
                     TextBox x = new TextBox();
-                    x.Text = i + " ; " + j;
+                   // x.Text = i + " ; " + j;
                     x.FontSize = 20;
                     Matrix.Children.Add(x);
                     Grid.SetColumn(x, j);
                     Grid.SetRow(x, i);
                     Ta[i, j] = x;
                 }
+                TextBox y = new TextBox();
+               // y.Text = i.ToString();
+                y.FontSize = 20;
+                Matrix.Children.Add(y);
+                Grid.SetColumn(y, 3);
+                Grid.SetRow(y, i);
+                Tb[i] = y;
             }
 
+           
+        }
+
+        private void Btn_Lösen_Click(object sender, RoutedEventArgs e)
+        {
+            double[,] GS = new double[3, 3];
+            double[] L  = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    GS[i, j] = Convert.ToDouble(Ta[i, j].Text);
+
+                }
+                L[i] = Convert.ToDouble(Tb[i].Text);
+            }
+            // Lösung berechnen
+            double p0, p1, g;
+           
+            for (int i = 0; i < 3; i++)
+            {
+                //evtl Zeilen tauschen
+                int Zähler = 0;
+                while (GS[i, i] == 0.0)
+                {
+                    if(Zähler == 3)
+                    {
+                        MessageBox.Show("Das Gleichungssystem hat keine eindeutige Lösung.");
+                        goto Ende;
+                    }
+                    ZeileTauschen(GS, L, i, Zähler);
+                    Ausgabe(GS, L);
+                    MessageBox.Show("Zeilen getauscht.");
+                    Zähler++;
+                }
+                g = GS[i, i];
+                p0 = GS[(i + 1) % 3, i];
+                p1 = GS[(i + 2) % 3, i];
+                for (int j = 0; j < 3; j++)
+                {
+                    GS[i, j] /= g;
+                    GS[(i + 1) % 3, j] += GS[i, j]*(-1)*p0;
+                    GS[(i + 2) % 3, j] += GS[i, j] * (-1) * p1;
+                }
+                L[i] /= g; 
+               
+                L[(i + 1) % 3] +=  L[i]*(-1)*p0;
+                L[(i + 2) % 3] += L[i] * (-1) * p1;
+                Ausgabe(GS, L);
+                MessageBox.Show("Schritt: " + i);
+            }
+        Ende:;
+            
+
+        }
+
+        private void Ausgabe (double [,] GSA, double [] LA)
+        {
+            for (int i = 0; i < 3; i++ )
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Ta[i, j].Text = GSA[i, j].ToString();
+                }
+                Tb[i].Text = LA[i].ToString(); 
+            }
+        }
+        private void ZeileTauschen (double[,] GSA, double[] LA, int zeile1, int zeile2)
+        {
+            double temp = 0;
+            for (int j = 0; j < 3; j++)
+            {
+                temp = GSA[zeile1, j];
+                GSA[zeile1, j] = GSA[zeile2, j];
+                GSA[zeile2, j] = temp;
+            }
+            temp = LA[zeile1];
+            LA[zeile1] = LA[zeile2];
+            LA[zeile2] = temp;
         }
     }
 }
